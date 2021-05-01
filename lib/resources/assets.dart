8b3c0f739eafe46fe8c5f08ff926efdde76d3711
RainVisitor <abc873693@gmail.com>
2021-05-01 18:56:52 +0800

    Add resources example

---
+++ lib/resources/assets.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class ImageAssets {
+  static const String basePath = 'assets/images';
+
+  // static const example = '$basePath/example.webp';
+}
+
+class FileAssets {
+  static const String basePath = 'assets';
+}
